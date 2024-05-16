4bfb8b36aa40275240c9fbf8d3ee1716763d172d
Mohamedbachir120 <mohamedbhadjadji@gmail.com>
2024-05-16 20:18:11 +0100

    add send otp api

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const app = express()
 
 app.use(express.json())
 
+export interface sendOtp {phoneNumber:string,apiKey:string}
 
 app.post(`/api/signup`, async (req, res) => {
 
@@ -73,6 +74,76 @@ app.get('/api/customers', AuthMiddleware ,async (req, res) => {
   res.json("test")
   // const users = await prisma.customers.findMany()
   // res.json(users)
+})
+app.post('/api/sendOtp', AuthMiddleware, async(req, res) => {
+
+  const code = Math.random() * (999999 - 100000) + 100000;
+  const {phoneNumber,apiKey}:sendOtp = req.body
+
+  
+
+  // get the api key 
+  const apiKeyId = await prisma.api_keys.findFirst({where:{Key: apiKey}})
+  // get operator  by checking the phone number format
+  let tempPhone  = phoneNumber.includes("213") ? phoneNumber.split('213')[1].charAt(0) : phoneNumber.charAt(1)
+  let operatorString:string  = "Ooredoo"; 
+  switch (tempPhone) {
+      case "6": operatorString = "Mobilis"; break;
+      case "7": operatorString = "Djezzy"; break;
+      default: operatorString ="Ooredoo"; break;
+
+  }
+  
+  let operator = await prisma.operators.findFirst({ where: { name: operatorString } })
+  if(operator != null) {
+
+    // getting the minimum price for the operator 
+    let minPrice = await prisma.operators_otp_providers.aggregate({
+      _min:{
+        base_price:true
+      },where:{
+        operator_id:operator.id
+      }
+    })
+    // getting the provider that has the min price
+    let perfectProvider =await  prisma.operators_otp_providers.findFirst({
+      where:{base_price: minPrice._min.base_price!}
+    })
+
+    let otpExpiration = new Date()
+    otpExpiration.setMinutes(otpExpiration.getMinutes() + 15)
+
+  // get the operation id 
+
+    const operation  = await prisma.operations.findFirst({where:{name:"SEND_SMS",AND:{
+      otp_provider_id:perfectProvider?.otp_provider_id
+    }},})
+    const result = await prisma.api_calls.create({
+      data: {
+        id:1  ,                    
+        api_key_id:apiKeyId?.id   ?? 0    ,       
+        operator_otp_provider_id:perfectProvider?.id ?? 0 , 
+        call_date :new Date(),              
+        operation_id   :operation?.id ?? 0 ,          
+        phone_number :phoneNumber    ,        
+        otp            :code.toString(),          
+        otp_validity  : otpExpiration  ,         
+        cost            :perfectProvider?.base_price ?? 0,       
+        Api_keys           :undefined,
+             
+        Operations       :undefined,        
+        Operators_otp_providers:undefined,
+      }
+     
+    })
+
+    // simulate the call to be added 
+
+    res.json({ "success": true, "message": "OTP Sent successfully" })
+  }
+
+
+
 })
 
 
