2315d5e623b199297e5649f430a9471498534d1e
Mohamedbachir120 <mohamedbhadjadji@gmail.com>
2024-05-18 16:58:49 +0100

    Add ApiKeyMiddleware && checkOtp Api endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,22 @@ import { Customers, Prisma, PrismaClient } from '@prisma/client'
 import express from 'express'
 import { AuthenticatedUser, CheckPassword, generateAccessToken, HashPassword, isAuthenticated } from './helpers/JwtHelper';
 import { CustomerToString } from './helpers/Serializers';
-import { AuthMiddleware } from './middleware/AuthMiddleware';
+import { ApiKeyMiddleware, AuthMiddleware } from './middleware/AuthMiddleware';
 
 export const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
 
-export interface sendOtp {phoneNumber:string,apiKey:string}
+export interface sendOtp { phoneNumber: string, apiKey: string }
 
 app.post(`/api/signup`, async (req, res) => {
 
 
 
-  const { name, email, password , company_name,nif,rc,} = req.body
+  const { name, email, password, company_name, nif, rc, } = req.body
 
-  const HashedPassword =  HashPassword(password);
+  const HashedPassword = HashPassword(password);
   const user = await prisma.customers.findFirst({ where: { email: email } });
 
   if (!user) {
@@ -27,10 +27,10 @@ app.post(`/api/signup`, async (req, res) => {
         name: name,
         email: email,
         password: HashedPassword,
-        company_name:company_name,
-        nif  :nif,
-        rc:rc,
-        balance:0,
+        company_name: company_name,
+        nif: nif,
+        rc: rc,
+        balance: 0,
       },
     })
     res.json({ "success": true, "message": "User created successfully" })
@@ -45,7 +45,7 @@ app.post(`/api/signin`, async (req, res) => {
   const user = await prisma.customers.findFirst({ where: { email: email } }) as Customers;
   const errorResponse = { "error": "Wrong email or password" };
   if (user) {
-    const passwordCorrect  =  CheckPassword(password, user.password);
+    const passwordCorrect = CheckPassword(password, user.password);
     if (!passwordCorrect) {
       res.status(401).json(errorResponse);
 
@@ -70,71 +70,71 @@ app.post(`/api/signin`, async (req, res) => {
 
 
 
-app.get('/api/customers', AuthMiddleware ,async (req, res) => {
+app.get('/api/customers', AuthMiddleware, async (req, res) => {
   res.json("test")
   // const users = await prisma.customers.findMany()
   // res.json(users)
 })
-app.post('/api/sendOtp', AuthMiddleware, async(req, res) => {
+app.post('/api/sendOtp', ApiKeyMiddleware, async (req, res) => {
 
   const code = Math.random() * (999999 - 100000) + 100000;
-  const {phoneNumber,apiKey}:sendOtp = req.body
-
-  
+  const { phoneNumber }: sendOtp = req.body
+  const apiKey = req.headers["key"] as string
 
   // get the api key 
-  const apiKeyId = await prisma.api_keys.findFirst({where:{Key: apiKey}})
+  const apiKeyId = await prisma.api_keys.findFirst({ where: { Key: apiKey } })
   // get operator  by checking the phone number format
-  let tempPhone  = phoneNumber.includes("213") ? phoneNumber.split('213')[1].charAt(0) : phoneNumber.charAt(1)
-  let operatorString:string  = "Ooredoo"; 
+  let tempPhone = phoneNumber.includes("213") ? phoneNumber.split('213')[1].charAt(0) : phoneNumber.charAt(1)
+  let operatorString: string = "Ooredoo";
   switch (tempPhone) {
-      case "6": operatorString = "Mobilis"; break;
-      case "7": operatorString = "Djezzy"; break;
-      default: operatorString ="Ooredoo"; break;
+    case "6": operatorString = "Mobilis"; break;
+    case "7": operatorString = "Djezzy"; break;
+    default: operatorString = "Ooredoo"; break;
 
   }
-  
+
   let operator = await prisma.operators.findFirst({ where: { name: operatorString } })
-  if(operator != null) {
+  if (operator != null) {
 
     // getting the minimum price for the operator 
     let minPrice = await prisma.operators_otp_providers.aggregate({
-      _min:{
-        base_price:true
-      },where:{
-        operator_id:operator.id
+      _min: {
+        base_price: true
+      }, where: {
+        operator_id: operator.id
       }
     })
     // getting the provider that has the min price
-    let perfectProvider =await  prisma.operators_otp_providers.findFirst({
-      where:{base_price: minPrice._min.base_price!}
+    let perfectProvider = await prisma.operators_otp_providers.findFirst({
+      where: { base_price: minPrice._min.base_price! }
     })
 
     let otpExpiration = new Date()
     otpExpiration.setMinutes(otpExpiration.getMinutes() + 15)
 
-  // get the operation id 
+    // get the operation id 
 
-    const operation  = await prisma.operations.findFirst({where:{name:"SEND_SMS",AND:{
-      otp_provider_id:perfectProvider?.otp_provider_id
-    }},})
+
+    const operation = await prisma.operations.findFirst({
+      where: {
+        name: "SEND_SMS", AND: {
+          otp_provider_id: perfectProvider?.otp_provider_id
+        }
+      },
+    })
     const result = await prisma.api_calls.create({
       data: {
-        id:1  ,                    
-        api_key_id:apiKeyId?.id   ?? 0    ,       
-        operator_otp_provider_id:perfectProvider?.id ?? 0 , 
-        call_date :new Date(),              
-        operation_id   :operation?.id ?? 0 ,          
-        phone_number :phoneNumber    ,        
-        otp            :code.toString(),          
-        otp_validity  : otpExpiration  ,         
-        cost            :perfectProvider?.base_price ?? 0,       
-        Api_keys           :undefined,
-             
-        Operations       :undefined,        
-        Operators_otp_providers:undefined,
+        api_key_id: apiKeyId?.id ?? 0,
+        operator_otp_provider_id: perfectProvider?.id ?? 0,
+        call_date: new Date(),
+        operation_id: operation?.id ?? 0,
+        phone_number: phoneNumber,
+        otp: Math.round(code).toString(),
+        otp_validity: otpExpiration,
+        cost: perfectProvider?.base_price ?? 0,
+
       }
-     
+
     })
 
     // simulate the call to be added 
@@ -146,11 +146,42 @@ app.post('/api/sendOtp', AuthMiddleware, async(req, res) => {
 
 })
 
+app.post('/api/checkOtp', ApiKeyMiddleware, async (req, res) => {
+  const apiKey = req.headers["key"] as string
+  const { code }: { code: string } = req.body
 
+
+  const apiCall = await prisma.api_calls.findFirst({
+    where: {
+      otp: code,
+      AND: {
+        AND: {
+          otp_validity: {
+            gt: new Date()
+          }
+        },
+        Api_keys: {
+          Key: apiKey
+        },
+      }
+    },
+
+  })
+  if (apiCall != null) {
+
+    res.json({ "success": true, "message": "OTP checked successfully" })
+  } else {
+
+    res.json({ "success": false, "message": "Wrong code" })
+
+  }
+
+
+});
 
 const server = app.listen(3000, () => {
 
-  
+
   console.log(`
 🚀 Server ready at: http://localhost:3000
 ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`);
--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -1,25 +1,53 @@
-import { NextFunction,Response,Request } from "express";
+import { NextFunction, Response, Request } from "express";
 import { isAuthenticated } from "../helpers/JwtHelper";
+import { prisma } from "..";
 
 
-export function AuthMiddleware(req: Request, res:  Response, next: NextFunction): void {
-    const bearerToken = req.headers["authorization"] 
-    if(typeof bearerToken  !== "undefined"){
+export function AuthMiddleware(req: Request, res: Response, next: NextFunction): void {
+    const bearerToken = req.headers["authorization"]
+    if (typeof bearerToken !== "undefined") {
         try {
-            const bearer = bearerToken?.split(" ") ?? ["",""]
-            if(!isAuthenticated(bearer[1])){
+            const bearer = bearerToken?.split(" ") ?? ["", ""]
+            if (!isAuthenticated(bearer[1])) {
                 res.sendStatus(403);
 
-            }else{
+            } else {
 
                 next();
             }
-            
+
         } catch (error) {
-        res.sendStatus(403);
-            
+            res.sendStatus(403);
+
         }
-    }else{
+    } else {
         res.sendStatus(403);
     }
-  }
+}
+
+export async function ApiKeyMiddleware(req: Request, res: Response, next: NextFunction) {
+    const key = req.headers["key"] as string
+
+    try {
+        if (typeof key !== "undefined") {
+
+            const apiKey = await prisma.api_keys.findFirst({ where: { Key: key } })
+
+            if (apiKey == null || new Date() > apiKey.valide_untill) {
+                res.sendStatus(403);
+
+            } else {
+
+                next();
+            }
+
+        }
+        else {
+            res.sendStatus(403);
+        }
+    }
+    catch (error) {
+        res.sendStatus(403);
+
+    }
+}
